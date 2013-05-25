4cfe095ea891a2e91b450ab87e2aeecd67c6c197
Benjamin Doherty <bjd@bangpound.org>
2013-05-25 05:58:36 -0500

    Media formats can be cached again.
    Encode the media filter tag into an inline macro that is rendered when the content is viewed.

---
+++ lib/Drupal/bangpoundmedia/MediaFilterMacro.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\bangpoundmedia\MediaFilterMacro.
+ */
+
+namespace Drupal\bangpoundmedia;
+
+use Drupal\inline\MacroBase;
+
+class MediaFilterMacro extends MacroBase {
+
+  protected $macroType = 'media_filter';
+
+  protected $tag;
+
+  public function getParameters() {
+    $args = array();
+    $args['tag'] = array(
+      '#datatype' => 'string',
+      '#required' => TRUE,
+      '#title' => t('Tag'),
+    );
+    return $args;
+  }
+
+  public function validate(array $context) {
+    if (empty($this->params['tag'])) {
+      return t('A media filter tag is required');
+    }
+    if (!base64_decode($this->params['tag'], TRUE)) {
+      return t('The tag paramter is not valid base64.');
+    }
+
+    // TODO: Also check for validity of JSON?
+  }
+
+  public function prepareView(array $context) {
+    module_load_include('inc', 'media', 'includes/media.filter');
+    $this->tag = base64_decode($this->params['tag']);
+  }
+
+  public function view(array $context) {
+    return media_token_to_markup(array($this->tag));
+  }
+}
